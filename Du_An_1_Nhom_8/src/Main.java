25953298a9ae879ece66aa6e069fac44694b9434
ngtnthori03 <ngtnthori03@gmail.com>
2024-03-23 15:41:48 +0700

    validate JTextField

--- Du_An_1_Nhom_8/src/Main.java
+++ Du_An_1_Nhom_8/src/Main.java
@@ -1,20 +1,27 @@
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
-import repository.VoucherRepository;
-import view.VoucherPanel;
+import View.VoucherPanel;
 
 /*
  * @author ngtnthori03
- * 
- * entrypoint
  */
 public class Main {
 
     public static void main(String[] args) {
-
-
-
+        
+        JFrame f = new JFrame();
+        
+        f.add(new VoucherPanel());
+        
+        f.setSize(500, 500);
+        
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        f.setLocationRelativeTo(null);
+        
+        f.setVisible(true);
+        
     }
 
 }
