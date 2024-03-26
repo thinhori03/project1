85411a29a5591a546d972da23aaf665a2d666c43
Thinh Nguyen <149649225+ngtnthori03@users.noreply.github.com>
2024-03-27 04:34:11 +0700

    Update VoucherFilter.java

--- Du_An_1_Nhom_8/src/dto/VoucherFilter.java
+++ Du_An_1_Nhom_8/src/dto/VoucherFilter.java
@@ -3,21 +3,31 @@ package dto;
 /**
  * @author ngtnthori03
 */
-@DataParam()
 public class VoucherFilter {
+    
+    @DataParam(name = "vid")
     private String voucherId;
 
+    @DataParam(name = "minPrice")
     private int minimumPrice;
+    @DataParam(name = "maxPrice")
     private int maximumPrice;
 
+    @DataParam(name = "minCondPrice")
     private int minimumComdPrice;
+    @DataParam(name = "maxCondPrice")
     private int maximumCondPrice;
 
+    @DataParam(name = "minStartDate")
     private Date minimumStartDate;
+    @DataParam(name = "maxStartDate")
     private Date maximumStartDate;
 
+    @DataParam(name = "minEndDate")
     private Date minimumEndDate;
+    @DataParam(name = "maxEndDate")
     private Date maximumEndDate;
 
+    @DataParam(name = "trangThai")
     private TrangThaiVoucher voucherStatus;
 }
