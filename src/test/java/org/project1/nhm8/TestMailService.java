01bae2fc66a5cf4ee4e351db1116dc23c38a55c2
ngtnthori03 <ngtnthori03@gmail.com>
2024-04-05 01:08:33 +0700

    HTML-resolver send-mail using HTML format

--- src/test/java/org/project1/nhm8/TestMailService.java
+++ src/test/java/org/project1/nhm8/TestMailService.java
@@ -3,13 +3,19 @@ package org.project1.nhm8;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.project1.nhom8.service.MailService;
+import org.project1.nhom8.util.HTMLResolver;
+import org.thymeleaf.context.Context;
 
 public class TestMailService {
 
     private final MailService mailService;
 
+    private final HTMLResolver htmlResolver;
+
     public TestMailService() {
         mailService = new MailService();
+
+        htmlResolver = new HTMLResolver();
     }
 
     @Test
@@ -17,9 +23,13 @@ public class TestMailService {
 
         mailService.autoAuth();
 
+        Context context = new Context();
+
+        context.setVariable("code", 123456);
+
         mailService.send("ngtnthori03@gmail.com"
                 , "forgot password code"
-                , "your code: 19762"
+                , htmlResolver.revolve("forgot-password", context)
         );
 
         Assertions.assertTrue(mailService.isResult());
