6961cc66bd54da11c0190ce36775a614a00bf96a
ngtnthori03 <ngtnthori03@gmail.com>
2024-04-05 16:41:11 +0700

    add itextpdf dependencies

--- src/main/java/org/project1/nhom8/Main.java
+++ src/main/java/org/project1/nhom8/Main.java
@@ -1,11 +1,5 @@
 package org.project1.nhom8;
 
-import java.awt.Button;
-import javax.swing.*;
-import org.project1.nhom8.View.LoadingPanel;
-import org.project1.nhom8.service.MailService;
-
-
 /*
  * @author ngtnthori03
  */
@@ -13,14 +7,14 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        try {
+//        try {
+//
+//            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatDarkLaf");
+//
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
 
-            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatDarkLaf");
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        
 
         new org.project1.nhom8.View.LoginDialog().setVisible(true);
     }
